60d37f37d6f3f6937122fc5a066ca79e9c39f747
mramazzini <mramazzini123@gmail.com>
2024-08-03 20:25:05 -0500

    opengraph test

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -4,6 +4,19 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Max's DND Wiki",
   description: "A collection of DND resources",
+  openGraph: {
+    type: "website",
+    title: "Max's DND Wiki",
+    description: "A collection of DND resources",
+    images: [
+      {
+        url: "/images/hero.jpg",
+        width: 1440,
+        height: 1920,
+        alt: "Description of the image",
+      },
+    ],
+  },
 };
 
 export default function Home() {
