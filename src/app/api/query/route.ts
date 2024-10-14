778fb57526593d3b96c036df1ec14123c66992f7
mramazzini <mramazzini123@gmail.com>
2024-10-14 16:15:25 -0500

    made route for globalsearch

---
+++ src/app/api/query/route.ts
@@ -0,0 +1,27 @@
+import { searchEverything } from "@/lib/actions/db/general/read.actions";
+import { QueryParams } from "@/lib/utils/types/types";
+
+export async function GET(req: Request) {
+  const params = new URL(req.url).searchParams;
+  const query = params.get("query");
+
+  const queryParams: QueryParams = {
+    query: query || "",
+    page: 0,
+    searchFields: [],
+    relationalFields: [],
+  };
+
+  const res = await searchEverything(queryParams);
+
+  if (!res || res.length === 0) {
+    return new Response("No results found", { status: 404 });
+  }
+
+  return new Response(JSON.stringify(res), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
