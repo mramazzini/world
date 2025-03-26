fa8c63150432a5e5288918d4db883b613fb805a0
mramazzini <mramazzini123@gmail.com>
2025-03-25 21:32:41 -0500

    Spell Ui

---
+++ src/components/CharacterSheet/Spells/SpellSelect.tsx
@@ -0,0 +1,109 @@
+import { SpellInfo } from '@/lib/types/modelInfo';
+import { SpellID, SpellLevel } from '@/lib/types/types';
+import { memoizeGetSpell } from '@/Utility/Indexed/globalCache';
+import { useEffect, useState } from 'react';
+import SpellSlotSelect from './SpellSlotSelect';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { setCurrentSpellSlots } from '@/store/sheetSlice';
+import { toSpellLevel } from '@/lib/utils/toSpellLevel';
+import Image from 'next/image';
+
+interface SpellSelectProps {
+  spell: SpellID;
+  selectSpell: (spellId: SpellID) => void;
+}
+
+const SpellSelect = ({ spell, selectSpell }: SpellSelectProps) => {
+  const [spellData, setSpellData] = useState<SpellInfo | null>(null);
+  const [displayPrep, setDisplayPrep] = useState(false);
+  const dispatch = useAppDispatch();
+  const { currentSpellSlots } = useAppSelector((state) => state.sheet);
+  const [spellLevel, setSpellLevel] = useState<SpellLevel>(0 as SpellLevel);
+
+  useEffect(() => {
+    const fetchSpell = async () => {
+      const res = await memoizeGetSpell({
+        query: spell,
+        type: 'id',
+      });
+      setSpellData(res);
+    };
+    fetchSpell();
+  }, [spell]);
+
+  const castSpell = (level: SpellLevel) => {
+    if (currentSpellSlots[level] === undefined) return;
+    if (currentSpellSlots[level] === 0) return;
+    dispatch(
+      setCurrentSpellSlots({
+        ...currentSpellSlots,
+        [level]: currentSpellSlots[level] - 1,
+      })
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-start justify-start bg-base-300 rounded-xl w-full p-4 gap-4">
+      <div className="flex flex-row items-center justify-start bg-base-300 rounded-xl w-full ">
+        <div className="tooltip font-bold" data-tip="Read More">
+          <button
+            onClick={() => selectSpell(spell)}
+            className="btn btn-ghost border border-gray-500"
+          >
+            <h3 className="text-lg ">{spellData?.name}</h3>
+          </button>
+        </div>
+        <div className="divider divider-vertical flex-grow px-4"></div>
+        <div className="flex flex-row gap-2 items-center justify-start ml-auto">
+          {spellLevel === spellData?.level ? (
+            <button
+              className="btn btn-ghost border border-gray-500"
+              onClick={() => setDisplayPrep(!displayPrep)}
+            >
+              Slot: {toSpellLevel(spellLevel ?? 0)}
+            </button>
+          ) : (
+            <button
+              className="btn btn-ghost border border-gray-500 relative bg-red-800 bg-opacity-50 overflow-hidden"
+              onClick={() => setDisplayPrep(!displayPrep)}
+            >
+              <Image
+                src="/gifs/fire-pixel.gif"
+                width={20}
+                height={20}
+                className="absolute w-full  bottom-0 right-0 z-0 opacity-100"
+                alt="asdasd"
+              />
+              <p className="relative z-1 bg-black bg-opacity-75 p-1 rounded-full  text-white">
+                Upcasting: {toSpellLevel(spellLevel ?? 0)}
+              </p>
+            </button>
+          )}
+          <button
+            className="btn btn-ghost border border-gray-500 ml-auto"
+            onClick={() => castSpell(spellLevel)}
+          >
+            Cast
+          </button>
+        </div>
+      </div>
+      <p className="text-sm">Casting Time: {spellData?.castingTime}</p>
+      <p className="text-sm">Range: {spellData?.range}</p>
+      <p className="text-sm">
+        Components: {spellData?.verbal && 'V'} {spellData?.somatic && 'S'}{' '}
+        {spellData?.material && 'M'} {spellData?.materialCost}
+      </p>
+      <p className="text-sm">Duration: {spellData?.duration}</p>
+
+      {spellData && displayPrep && (
+        <SpellSlotSelect
+          spell={spellData}
+          selectedSlot={spellLevel}
+          setSelectedSlot={setSpellLevel}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SpellSelect;
--- src/components/CharacterSheet/Spells/SpellSheet.tsx
+++ src/components/CharacterSheet/Spells/SpellSheet.tsx
@@ -1,7 +1,12 @@
-import { SpellLevel } from '@/lib/types/types';
+import { SpellInfo } from '@/lib/types/modelInfo';
+import { SpellID, SpellLevel } from '@/lib/types/types';
 import numberArray from '@/lib/utils/numberArray';
 import numPlace from '@/lib/utils/numPlace';
+import SpellPage from '@/pages-lib/wiki/official/Spell.page';
 import { useAppSelector } from '@/store/hooks';
+import { useEffect, useState } from 'react';
+import SpellSelect from './SpellSelect';
+import { memoizeGetSpell } from '@/Utility/Indexed/globalCache';
 import P from '@/Utility/FormatAndSanitize';
 
 //Show free spells
@@ -12,11 +17,26 @@ import P from '@/Utility/FormatAndSanitize';
 const SpellSheet = () => {
   const { spellSlots, currentSpellSlots, freeSpells, preparedSpells } =
     useAppSelector((state) => state.sheet);
+  const [selectedSpellId, setSelectedSpellId] = useState<SpellID | null>(null);
+  const [selectedSpell, setSelectedSpell] = useState<SpellInfo | null>(null);
+
+  useEffect(() => {
+    const setSpell = async () => {
+      if (!selectedSpellId) return;
+      const spell = await memoizeGetSpell({
+        query: selectedSpellId,
+        type: 'id',
+      });
+      setSelectedSpell(spell);
+    };
+    console.log(currentSpellSlots);
+    setSpell();
+  }, [preparedSpells, selectedSpellId, currentSpellSlots]);
   return (
-    <div className="grid grid-cols-2 gap-4 bg-base-200 p-4 rounded-xl">
+    <div className="grid grid-cols-2 gap-4 bg-base-200 p-4 rounded-xl ">
       <h2 className="divider bg-base-300 p-4 m-0 col-span-2">Spell Slots</h2>
       <div
-        className={`flex flex-wrap gap-4  items-center justify-center w-full col-span-2`}
+        className={`flex flex-wrap gap-4  items-center justify-center w-full col-span-2 `}
       >
         {Object.entries(spellSlots).map(([level, slots]) => (
           <div
@@ -41,40 +61,51 @@ const SpellSheet = () => {
           </div>
         ))}
       </div>
-      <div className="bg-base-300 p-4 rounded-xl">
-        <h2 className="divider  mt-0">Free Spells</h2>
-        <p>Spells you can cast without expending a spell slot.</p>
-        {freeSpells.map((spellId) => (
-          <div
-            key={spellId}
-            className="flex flex-col gap-2 items-center justify-center bg-base-300 p-4 rounded-xl"
-          >
-            <p className=" p-4">
-              {' '}
-              <P>
-                %{spellId}
-                {`{}`}%
-              </P>
-            </p>
+      <div className="bg-base-300 p-4 rounded-xl col-span-1 border border-primary max-h-[986px] overflow-scroll">
+        {selectedSpell ? (
+          <SpellPage spell={selectedSpell} />
+        ) : (
+          <div className="h-32 p-4 flex justify-center items-center">
+            <h3 className="divider bg-base-200 w-full px-2 rounded-xl">
+              No Spell Selected
+            </h3>
           </div>
-        ))}
+        )}
       </div>
-      <div className="bg-base-300 p-4 rounded-xl">
-        <h2 className="divider mt-0">Prepared Spells</h2>
-        <p>Spells you have prepared and can cast by expending a spell slot.</p>
-        {preparedSpells.map((spellId) => (
-          <div
-            key={spellId}
-            className="flex flex-col gap-2 items-center justify-center bg-base-300 p-4 rounded-xl"
-          >
-            <p className="p-4 ">
+      <div className="bg-base-300 p-4 rounded-xl gap-4 flex flex-col max-h-[986px] overflow-scroll border border-primary">
+        {freeSpells.length > 0 && (
+          <div className="bg-base-200 p-4 rounded-xl flex gap-4 flex-col">
+            <h2 className="divider  mt-0">Free Spells</h2>
+            <p className="text-center">
+              <P>Spells you can cast without expending a spell slot.</P>
+            </p>
+            {freeSpells.map((spellId) => (
+              <SpellSelect
+                key={spellId}
+                spell={spellId}
+                selectSpell={setSelectedSpellId}
+              />
+            ))}
+          </div>
+        )}
+        {preparedSpells.length > 0 && (
+          <div className="bg-base-200 p-4 rounded-xl flex gap-4 flex-col">
+            <h2 className="divider mt-0">Prepared Spells</h2>
+            <p className="text-center">
               <P>
-                %{spellId}
-                {`{}`}%
+                Spells you have prepared and can cast by expending a spell slot.
               </P>
             </p>
+
+            {preparedSpells.map((spellId) => (
+              <SpellSelect
+                key={spellId}
+                spell={spellId}
+                selectSpell={setSelectedSpellId}
+              />
+            ))}
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
